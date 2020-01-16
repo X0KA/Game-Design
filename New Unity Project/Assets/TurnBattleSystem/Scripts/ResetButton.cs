9a3ffd06bf0a5425c99150682265733a439a35c1
Joan Marín <36265613+X0KA@users.noreply.github.com>
2020-01-16 15:56:13 +0100

    Added reset button (doesn't work yet)

---
+++ New Unity Project/Assets/TurnBattleSystem/Scripts/ResetButton.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetButton : MonoBehaviour
+{
+    public GameObject button;
+    private bool active=false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        button.SetActive(active);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {  
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            active = !active;
+            button.SetActive(active);
+        }
+    }
+}
