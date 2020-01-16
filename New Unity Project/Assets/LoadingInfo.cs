a4b220d46edd51995eaebd75dbe1ae59b71da175
Roger Leon Borras <36153923+rleonborras@users.noreply.github.com>
2020-01-17 00:33:01 +0100

    Added selectable enemy unitys

--- New Unity Project/Assets/LoadingInfo.cs
+++ New Unity Project/Assets/LoadingInfo.cs
@@ -5,11 +5,27 @@ using UnityEngine;
 public class LoadingInfo : MonoBehaviour
 {
 
+    public enum EnemyTypes
+    {
+        Guard,
+        Damicist,
+        Protector,
+        Amadea
+    }
+
     public GameObject Admon;
     public GameObject Florence;
     public GameObject Claudio;
 
+    public EnemyTypes[] EnemyList;
 
+    private void Start()
+    {
+        EnemyList = new EnemyTypes[3];
+        EnemyList[0] = EnemyTypes.Guard;
+        EnemyList[1] = EnemyTypes.Guard;
+        EnemyList[2] = EnemyTypes.Guard;
+    }
     public bool PlayerVsBot;
     // Start is called before the first frame update
 
@@ -31,18 +47,46 @@ public class LoadingInfo : MonoBehaviour
     public void SetAdmonLvl(string Level)
     {
         int lvl = int.Parse(Level);
+        
         Admon.GetComponent<Stats>().AdmonSetStatsbyLevel(lvl);
     }
 
     public void SetClaudioLvl(string Level)
     {
         int lvl = int.Parse(Level);
+
         Claudio.GetComponent<Stats>().AdmonSetStatsbyLevel(lvl);
     }
     public void SetFlorenceLvl(string Level)
     {
         int lvl = int.Parse(Level);
+
         Florence.GetComponent<Stats>().AdmonSetStatsbyLevel(lvl);
     }
+
+    public void CheckMaxandMinLevel(string Level)
+    {
+
+        int lvl = int.Parse(Level);
+        if (lvl > 10) lvl = 10;
+        if (lvl <= 0) lvl = 1;
+
+        Level = lvl.ToString();
+        
+    }
+
+    public void SetEnemeyOne(int index)
+    {
+        EnemyList[0] = (EnemyTypes)index;
+    }
+    public void SetEnemeyTwo(int index)
+    {
+        EnemyList[1] = (EnemyTypes)index;
+    }
+    public void SetEnemeyThree(int index)
+    {
+        EnemyList[2] = (EnemyTypes)index;
+    }
+
 
 }
